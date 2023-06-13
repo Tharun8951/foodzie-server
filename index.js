30edf988ea11f73da6fb5879f557cf08764d57fa
tharunbytes <tharunappu2004@gmail.com>
2023-06-13 09:00:43 +0530

    cors origin fix

--- index.js
+++ index.js
@@ -4,34 +4,34 @@ const cors = require('cors')
 const User = require('./Models/User')
 require('dotenv').config()
 
-app.use(cors({
+app.use(
+  cors({
     origin: [
-        "https://foodzie-client.vercel.app",
-        "http://localhost:3000"
-]
-}));
-
-
+      'http://localhost:3000',
+      'https://foodzie-client.vercel.app',
+    //   'https://mern-netclix-client.vercel.app',
+    ],
+  }),
+)
 
 app.use(express.json())
 
-app.listen(4040, ()=>{
-    console.log('server started at port 4040')
+app.listen(4040, () => {
+  console.log('server started at port 4040')
 })
 
 const mongoDB = require('./db')
 mongoDB()
 
-
-app.get('/', (req,res)=>{
-    res.send('Hello world')
+app.get('/', (req, res) => {
+  res.send('Hello world')
 })
 
 app.use('/api', require('./Routes/CreateUser'))
-app.use('/api', require('./Routes/displayData.js'))
-app.get('/all', async (req,res)=>{
-    const alldata = await User.find({})
-    res.json({
-        data: alldata
-    })
+app.use('/api', require('./Routes/displayData'))
+app.get('/all', async (req, res) => {
+  const alldata = await User.find({})
+  res.json({
+    data: alldata,
+  })
 })
